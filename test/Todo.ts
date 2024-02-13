19849ba2b7966dd2563abd3fb3c32594c9168d2c
mubarakOke <okeolamubarak18@gmail.com>
2024-02-13 15:44:58 +0100

    create and get todo test

--- test/Todo.ts
+++ test/Todo.ts
@@ -7,7 +7,7 @@ import {
   import { ethers } from "hardhat";
   
   describe("Lock", function () {
-    async function deployOneYearLockFixture() {  
+    async function deployToDo() {  
       // Contracts are deployed using the first signer/account by default
       const [owner, otherAccount] = await ethers.getSigners();
   
@@ -18,35 +18,16 @@ import {
     }
   
     describe("Deployment", function () {
-      it("Should set the right unlockTime", async function () {
-        const { toDo, unlockTime } = await loadFixture(deployOneYearLockFixture);
-  
-        expect(await lock.unlockTime()).to.equal(unlockTime);
-      });
-  
-      it("Should set the right owner", async function () {
-        const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-  
-        expect(await lock.owner()).to.equal(owner.address);
-      });
-  
-      it("Should receive and store the funds to lock", async function () {
-        const { lock, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-  
-        expect(await ethers.provider.getBalance(lock.target)).to.equal(
-          lockedAmount
-        );
-      });
-  
-      it("Should fail if the unlockTime is not in the future", async function () {
-        // We don't use the fixture here because we want a different deployment
-        const latestTime = await time.latest();
-        const Lock = await ethers.getContractFactory("Lock");
-        await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-          "Unlock time should be in the future"
-        );
+      it("Should create and get todo", async function () {
+        const { toDo } = await loadFixture(deployToDo);
+        const title= "test title";
+        const description= "test description";
+
+        const tx= await toDo.createTodo(title, description)
+        const returnedTodo= await toDo.getTodo(0)
+
+        expect(returnedTodo[2]).to.equal(title);
+        expect(returnedTodo[3]).to.equal(description);
       });
     });
   });
